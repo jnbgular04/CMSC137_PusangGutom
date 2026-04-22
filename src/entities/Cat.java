079065a6d8d0612c2accf732eab90f4ab742bf6f
Steven Toledo <mptoledo2@up.edu.ph>
2026-04-22 18:36:46 +0800

    feat: setup skeleton classes for milestone 1 areas of responsibility

---
+++ src/entities/Cat.java
@@ -0,0 +1,8 @@
+package entities;
+
+public class Cat {
+    public int x, y;
+    public int score;
+    public String name;
+    // For MS2, this will eventually hold the player's unique ID
+}
---
+++ src/entities/Mouse.java
@@ -0,0 +1,7 @@
+package entities;
+
+public class Mouse {
+    public int x, y;
+    public boolean isActive; // Is the mouse currently on screen?
+    public int radius = 25;  // Size for collision detection [cite: 43]
+}

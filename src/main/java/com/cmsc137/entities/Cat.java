d98bd01882f7862ac9368e790455d77211c75981
‘Yanika <yjtauro@up.edu.ph>
2026-05-20 23:04:26 +0800

    feat: added animation variables

--- src/main/java/com/cmsc137/entities/Cat.java
+++ src/main/java/com/cmsc137/entities/Cat.java
@@ -4,5 +4,13 @@ public class Cat {
     public int x, y;
     public int score;
     public String name;
+    
+    // Animation state variables
+    public int animTargetX;
+    public int animTargetY;
+    public boolean isAnimating;
+    
+    
     // For MS2, this will eventually hold the player's unique ID
+    public int id;
 }
